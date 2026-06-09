d0c5c5f05f7434806920eea012961d2eba3be324
Rodrigo Ramos <rodrigoramos@MacBook-de-Rodrigo.local>
2026-06-09 11:02:23 -0300

    Update public sharing provider to serveo.net for higher stability

--- compartilhar_terminal.sh
+++ compartilhar_terminal.sh
@@ -6,4 +6,4 @@ echo "Porta local: 5006"
 echo "Pressione CTRL+C a qualquer momento para fechar o túnel."
 echo "=========================================================="
 echo ""
-ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=30 -R 80:localhost:5006 nokey@localhost.run
+ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=30 -R 80:localhost:5006 serveo.net
